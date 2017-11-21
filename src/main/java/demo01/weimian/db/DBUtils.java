a0f88e08b67a4b75d01bdd06a890fbe748b6c6c3
qiuweimian <931503200@qq.com>
2017-11-21 21:36:03 +0800

    伟绵提交

---
+++ src/main/java/demo01/weimian/db/DBUtils.java
@@ -0,0 +1,22 @@
+package demo01.weimian.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBUtils {
+    private static final String URL="jdbc:mysql://192.168.128.130/test";
+    private static final String USER="root";
+    private static final String PASSWORD="root";
+    private static Connection conn=null;
+    public static Connection getConn(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn=DriverManager.getConnection(URL,USER,PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
