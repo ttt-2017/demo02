c698a1f782fce4811443c950d12a347590e35601
Er1995 <752106525@qq.com>
2017-11-21 18:09:04 +0800

    class User

---
+++ src/main/java/demo01/User.java
@@ -0,0 +1,34 @@
+package demo01;
+
+/**
+ * Created by Er on 2017/11/21.
+ */
+public class User {
+    private String name;
+    private int age;
+    private int id;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
