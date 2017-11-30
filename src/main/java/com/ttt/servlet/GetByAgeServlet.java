ec12d254be1ac9ceca8bb7e9bc356a2779e60be6
qiuweimian <931503200@qq.com>
2017-11-30 09:31:57 +0800

    重新提交GetByAgeServlet.java，GetByIdServlet.java，getUserByAge.jsp，getUserById.jsp

---
+++ src/main/java/com/ttt/servlet/GetByAgeServlet.java
@@ -0,0 +1,47 @@
+package com.ttt.servlet;
+
+import com.ttt.dao.UserDAOImpl;
+import com.ttt.model.User;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/11/29.
+ */
+public class GetByAgeServlet extends javax.servlet.http.HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charaset=UTF-8");
+        String age=request.getParameter("age");
+        String age1=request.getParameter("age1");
+        PrintWriter out=response.getWriter();
+        List<User> userList=new UserDAOImpl().getUserByAge(Integer.parseInt(age),Integer.parseInt(age1));
+        if(userList.size()>0){
+            out.println("<table border=1 align='center'>");
+            out.println("<tr><td>用户Id</td><td>用户名称</td><td>用户年龄</td></tr>");
+            for (User user : userList) {
+                out.print("<tr>");
+                out.print("<td>"+user.getId()+"</td>");
+                out.print("<td>"+user.getName()+"</td>");
+                out.print("<td>"+user.getAge()+"</td>");
+                out.print("</tr>");
+            }
+            out.print("</table>");
+        }else {
+            out.print("该用户不存在！");
+        }
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+
+    }
+}
---
+++ src/main/java/com/ttt/servlet/GetByIdServlet.java
@@ -0,0 +1,43 @@
+package com.ttt.servlet;
+
+import com.ttt.dao.UserDAOImpl;
+import com.ttt.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Administrator on 2017/11/29.
+ */
+public class GetByIdServlet extends javax.servlet.http.HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charaset=utf-8");
+        String id=request.getParameter("id");
+        User user=new UserDAOImpl().getUserById(Integer.parseInt(id));
+        PrintWriter out = response.getWriter();
+        if(user.getId()!=0 && !user.getName().equals(null) && user.getAge()!=0){
+            out.println("<table border=1 align='center'>");
+            out.println("<tr><td>用户Id</td><td>用户名称</td><td>用户年龄</td></tr>");
+            out.print("<tr><td>"+user.getId()+"</td>"+"<td>"+user.getName()+"</td>"+"<td>"+user.getAge()+"</td>"+"</tr>");
+            out.print("</table>");
+        }else{
+            out.print("不存在用户Id为"+id+"的用户！");
+        }
+
+
+
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+
+    }
+}
